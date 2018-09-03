9312db8374f65117c2758c94066a00aa7b55c530
Tarun Goyal <tarun13317@gmail.com>
2018-09-03 20:50:56 +0530

    Heapify logic modified and compact but still failing at some cases

--- Heap_Sort/main.c
+++ Heap_Sort/main.c
@@ -19,27 +19,26 @@ void swap(int *a, int *b){
 void restore(int * heap, int index){
     if(index<1)return;
 
-    int * parent = heap+index;
+    int * current = heap+index;
+    int * parent = heap+index/2;
     int * left = heap+2*index;
     int * right = heap+(2*index+1);
 
 //    printf("%d, %d, %d, %d\n", *parent, *left, *right, *(heap+(index/2)));
 //    return;
 
-    //before checking for children check for its parent
     if(index>1){
-        if(*(heap+(index/2))<*parent){
-            swap(heap+(index/2), parent);
-        }
+        int * m1 = maximumNode(left, right);
+        if(*m1>*current && *m1>*parent)
+            swap(m1, parent);
+        if(*current>*parent)
+            swap(current, parent);
     }
-
-    //now check for children
-    if(*parent<*left||*parent<*right){
-        swap(maximumNode(left, right), parent);
-        restore(heap, index/2);
-    }
+    int * m2 = maximumNode(left, right);
+    if(*m2>*current)
+        swap(m2, current);
+    restore(heap, index/2);
 }
-
 
 void max_heapify(int * heap, int n){
     for(int i=1;i<=n;i++)printf("%d\t",*(heap+i));
@@ -53,19 +52,8 @@ int main(){
     printf("Enter the number elements for heap:");
     scanf("%d", &n);
     int * heap = (int *)malloc(n*sizeof(int));
-*(heap+1) = 25;
-*(heap+2) = 35;
-*(heap+3) = 18;
-*(heap+4) = 9;
-*(heap+5) = 46;
-*(heap+6) = 70;
-*(heap+7) = 48;
-*(heap+8) = 23;
-*(heap+9) = 78;
-*(heap+10) = 12;
-*(heap+11) = 95;
-//    printf("Enter %d elements in heap, separated by space or line by line:\n", n);
-//    for(int i=1;i<=n;i++)scanf("%d", heap+i);
+    printf("Enter %d elements in heap, separated by space or line by line:\n", n);
+    for(int i=1;i<=n;i++)scanf("%d", heap+i);
 
     max_heapify(heap, n);
 
